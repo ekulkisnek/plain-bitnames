LayerTwo-Labs/thunder-rust

update deps/deny.toml

app/gui/mod.rs
@@ -226,13 +226,13 @@ impl eframe::App for EguiApp {
         if let Some(app) = self.app.as_ref()
             && !app.wallet.has_seed().unwrap_or(false)
         {
-            egui::CentralPanel::default().show_inside(ui, |ui| {
+            egui::CentralPanel::default().show(ui, |ui| {
                 egui::Window::new("Set Seed").show(ui, |ui| {
                     self.set_seed.show(app, ui);
                 });
             });
         } else {
-            egui::Panel::top("tabs").show_inside(ui, |ui| {
+            egui::Panel::top("tabs").show(ui, |ui| {
                 ui.horizontal(|ui| {
                     Tab::iter().for_each(|tab_variant| {
                         let tab_name = tab_variant.to_string();
@@ -244,29 +244,26 @@ impl eframe::App for EguiApp {
                     })
                 });
             });
-            egui::Panel::bottom("bottom_panel").show_inside(ui, |ui| {
-                self.bottom_panel.show(&mut self.miner, ui)
-            });
-            egui::CentralPanel::default().show_inside(ui, |ui| {
-                match self.tab {
-                    Tab::ParentChain => {
-                        self.parent_chain.show(self.app.as_ref(), ui)
-                    }
-                    Tab::Coins => {
-                        self.coins.show(self.app.as_ref(), ui);
-                    }
-                    Tab::MemPoolExplorer => {
-                        self.mempool_explorer.show(self.app.as_ref(), ui);
-                    }
-                    Tab::BlockExplorer => {
-                        self.block_explorer.show(self.app.as_ref(), ui);
-                    }
-                    Tab::Withdrawals => {
-                        self.withdrawals.show(self.app.as_ref(), ui);
-                    }
-                    Tab::ConsoleLogs => {
-                        self.console_logs.show(self.app.as_ref(), ui);
-                    }
+            egui::Panel::bottom("bottom_panel")
+                .show(ui, |ui| self.bottom_panel.show(&mut self.miner, ui));
+            egui::CentralPanel::default().show(ui, |ui| match self.tab {
+                Tab::ParentChain => {
+                    self.parent_chain.show(self.app.as_ref(), ui)
+                }
+                Tab::Coins => {
+                    self.coins.show(self.app.as_ref(), ui);
+                }
+                Tab::MemPoolExplorer => {
+                    self.mempool_explorer.show(self.app.as_ref(), ui);
+                }
+                Tab::BlockExplorer => {
+                    self.block_explorer.show(self.app.as_ref(), ui);
+                }
+                Tab::Withdrawals => {
+                    self.withdrawals.show(self.app.as_ref(), ui);
+                }
+                Tab::ConsoleLogs => {
+                    self.console_logs.show(self.app.as_ref(), ui);
                 }
             });
         }

ekulkisnek/plain-bitnames

app/gui/mod.rs
@@ -242,13 +242,13 @@ impl eframe::App for EguiApp {
         if let Some(app) = self.app.as_ref()
             && !app.wallet.has_seed().unwrap_or(false)
         {
-            egui::CentralPanel::default().show_inside(ui, |ui| {
+            egui::CentralPanel::default().show(ui, |ui| {
                 egui::Window::new("Set Seed").show(ui, |ui| {
                     self.set_seed.show(app, ui);
                 });
             });
         } else {
-            egui::Panel::top("tabs").show_inside(ui, |ui| {
+            egui::Panel::top("tabs").show(ui, |ui| {
                 ui.horizontal(|ui| {
                     Tab::iter().for_each(|tab_variant| {
                         let tab_name = tab_variant.to_string();
@@ -260,33 +260,29 @@ impl eframe::App for EguiApp {
                     })
                 });
             });
-            egui::Panel::bottom("bottom_panel").show_inside(ui, |ui| {
-                self.bottom_panel.show(&mut self.miner, ui)
-            });
-            egui::CentralPanel::default().show_inside(ui, |ui| {
-                match self.tab {
-                    Tab::ParentChain => {
-                        self.parent_chain.show(self.app.as_ref(), ui)
-                    }
-                    Tab::Coins => {
-                        let () =
-                            self.coins.show(self.app.as_ref(), ui).unwrap();
-                    }
-                    Tab::BitNames => {
-                        self.bitnames.show(self.app.as_ref(), ui);
-                    }
-                    Tab::Paymail => {
-                        self.paymail.show(self.app.as_ref(), ui).unwrap()
-                    }
-                    Tab::Messaging => {
-                        self.messaging.show(self.app.as_ref(), ui);
-                    }
-                    Tab::Activity => {
-                        self.activity.show(self.app.as_ref(), ui);
-                    }
-                    Tab::ConsoleLogs => {
-                        self.console_logs.show(self.app.as_ref(), ui);
-                    }
+            egui::Panel::bottom("bottom_panel")
+                .show(ui, |ui| self.bottom_panel.show(&mut self.miner, ui));
+            egui::CentralPanel::default().show(ui, |ui| match self.tab {
+                Tab::ParentChain => {
+                    self.parent_chain.show(self.app.as_ref(), ui)
+                }
+                Tab::Coins => {
+                    let () = self.coins.show(self.app.as_ref(), ui).unwrap();
+                }
+                Tab::BitNames => {
+                    self.bitnames.show(self.app.as_ref(), ui);
+                }
+                Tab::Paymail => {
+                    self.paymail.show(self.app.as_ref(), ui).unwrap()
+                }
+                Tab::Messaging => {
+                    self.messaging.show(self.app.as_ref(), ui);
+                }
+                Tab::Activity => {
+                    self.activity.show(self.app.as_ref(), ui);
+                }
+                Tab::ConsoleLogs => {
+                    self.console_logs.show(self.app.as_ref(), ui);
                 }
             });
         }